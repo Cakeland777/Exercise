28df50b44357ea916b73d3d85acd6875cc0763d4
yujin <90889155+Cakeland777@users.noreply.github.com>
2022-01-14 14:48:07 +0900

    Create fibo.py

---
+++ python/fibo.py
@@ -0,0 +1,12 @@
+#https://www.acmicpc.net/problem/2747
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n== (1 or 2):
+        return 1
+    else:
+        return fibo(n-1)+fibo(n-2)
+
+N=int(input())
+print(fibo(N))
